768a1f341a6a9794217a33fb76e7ff0a1e0646ae
Timea Bartha <timea.bartha.93@gmail.com>
2023-08-21 16:26:37 +0200

    Added data controller

--- Flavorly/DataController.swift
+++ Flavorly/DataController.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import CoreData
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "RecipeDesign")
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core data failed to lead: \(error.localizedDescription)")
+            }
+        }
+    }
+}
+
