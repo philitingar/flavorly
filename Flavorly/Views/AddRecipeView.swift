85f09fd746d66ece33070d8a31ac793a153fd7b8
Timea Bartha <timea.bartha.93@gmail.com>
2023-08-21 16:31:49 +0200

    Created the addRecipeView
    Has a function Delete Recipe, and incorporates the hardnessvieew

---
+++ Flavorly/Views/AddRecipeView.swift
@@ -0,0 +1,119 @@
+//
+//  AddRecipeView.swift
+//  Flavorly
+//
+//  Created by Timea Bartha on 21/8/23.
+//
+import CoreData
+import SwiftUI
+
+struct AddRecipeView: View {
+    
+    @Environment(\.managedObjectContext) var moc //environment property to store our managed object context:
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var title = ""
+    @State private var type = ""
+    @State private var ingredients = ""
+    @State private var recipe = ""
+    @State private var author = ""
+    @State private var hardness = 3
+    @State private var diet = ""
+    @State private var occasion = ""
+    
+    let diets = ["Vegetarian", "Vegan", "Gluten free", "Pescatarian", "Omnivore"]
+    let types = ["Soup", "Salad", "Main", "Dessert"]
+    let occasions = ["Christmas", "New Years", "Birthday", "Easter"]
+    
+    var hasValidName: Bool {
+        if title.isEmpty || author.isEmpty {
+            return false
+        }
+        
+        return true
+    }
+    
+    var body: some View {
+        NavigationView {
+                Form {
+                    Section {
+                        TextField("Recipe name", text: $title)
+                        TextField("Author's name", text: $author)
+                        
+                        Picker("Diet", selection: $diet) {
+                            ForEach(diets, id: \.self) {
+                                Text($0)
+                            }
+                        }
+                        Picker("Type", selection: $type) {
+                            ForEach(types, id: \.self) {
+                                Text($0)
+                            }
+                        }
+                        Picker("Occasion", selection: $occasion) {
+                            ForEach(occasions, id: \.self) {
+                                Text($0)
+                            }
+                        }
+                        
+                    }
+                    
+                    Section {
+                        HardnessView(hardness: $hardness)
+                    } header: {
+                        Text("How hard is this?")
+                    }
+                    
+                    Section {
+                        TextEditor(text: $ingredients)
+                    } header: {
+                        Text("Write the ingredients")
+                    }
+                    Section {
+                        TextEditor(text: $recipe)
+                    } header: {
+                        Text("recipe")
+                    }
+                    Section {
+                        Button("Save") {
+                            let newRecipe = Recipe(context: moc)
+                            newRecipe.id = UUID()
+                            newRecipe.title = title
+                            newRecipe.author = author
+                            newRecipe.hardness = Int16(hardness)
+                            newRecipe.diet = diet
+                            newRecipe.occasion = occasion
+                            newRecipe.ingredients = ingredients
+                            newRecipe.type = type
+                            newRecipe.recipe = recipe
+                            
+                            try? moc.save()
+                            dismiss()
+                        }
+                        
+                    }
+                    .disabled(hasValidName == false)
+                }
+                .toolbar {
+                    ToolbarItemGroup(placement: .navigationBarLeading) {
+                        Button {
+                            dismiss()
+                        } label: {
+                            Label("Back", systemImage: "xmark")
+                        }
+                        Text("Add recipe".uppercased())
+                            .padding(95)
+                            
+                    }
+                }
+            }
+        }
+    
+    
+}
+
+struct AddRecipeView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddRecipeView()
+    }
+}
