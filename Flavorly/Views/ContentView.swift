27def55f7f391aff6b71a216e83aab72c31ffaf8
Timea Bartha <timea.bartha.93@gmail.com>
2023-08-21 16:33:09 +0200

    Finished contentview so the app is functional

--- Flavorly/Views/ContentView.swift
+++ Flavorly/Views/ContentView.swift
@@ -4,18 +4,74 @@
 //
 //  Created by Timea Bartha on 21/8/23.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: [
+        SortDescriptor(\.title),
+        SortDescriptor(\.author)
+    ]) var recipes: FetchedResults<Recipe>
+    
+    @State private var showingAddScreen = false
+    @State private var showingSearchScreen = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(recipes) { recipe in
+                    NavigationLink {
+                        DetailView(recipe: recipe)
+                    } label: {
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(recipe.title ?? "Unknown Title")
+                                    .font(.headline)
+                                Text(recipe.author ?? "Unknown Author")
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                    }
+                }
+                .onDelete(perform: deleteRecipe)
+            }
+                .navigationTitle("RecipeDesign")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        EditButton()
+                    }
+                    ToolbarItemGroup(placement: .navigationBarTrailing) {
+                        Button {
+                            self.showingAddScreen.toggle()
+                        } label: {
+                            Label("Add Recipe", systemImage: "plus")
+                        }
+                        Button {
+                            self.showingSearchScreen.toggle()
+                        } label: {
+                            Label("Search", systemImage: "magnifyingglass")
+                        }
+                    }
+                }
+                .sheet(isPresented: $showingAddScreen) {
+                    AddRecipeView()
+                }
+                .sheet(isPresented: $showingSearchScreen) {
+                    SearchView()
+                }
         }
-        .padding()
+    }
+    func deleteRecipe(at offsets: IndexSet) {
+        for offset in offsets {
+            // find this book in our fetch request
+            let recipe = recipes[offset]
+
+            // delete it from the context
+            moc.delete(recipe)
+        }
+
+        // save the context
+        try? moc.save()
     }
 }
 
