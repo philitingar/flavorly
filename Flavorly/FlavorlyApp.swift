61c3883cb1dfed4583d217bf00f1089bdb7286d3
Timea Bartha <timea.bartha.93@gmail.com>
2023-08-21 16:16:08 +0200

    Initial Commit

---
+++ Flavorly/FlavorlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlavorlyApp.swift
+//  Flavorly
+//
+//  Created by Timea Bartha on 21/8/23.
+//
+
+import SwiftUI
+
+@main
+struct FlavorlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
