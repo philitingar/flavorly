db208e02731760e78d49dba5716c93c5ec5520fe
Timea Bartha <timea.bartha.93@gmail.com>
2023-08-21 16:32:53 +0200

    Created the search view to search for recipes

--- Flavorly/Views/SearchView.swift
+++ Flavorly/Views/SearchView.swift
@@ -4,12 +4,43 @@
 //
 //  Created by Timea Bartha on 21/8/23.
 //
-
+import CoreData
 import SwiftUI
 
 struct SearchView: View {
+    let searchObjects = [ "diet", "occasion", "type" ]
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.dismiss) var dismiss
+    @FetchRequest(sortDescriptors: [])
+    private var recipes: FetchedResults<Recipe>
+    
+    @State private var searchText = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                ForEach(recipes, id: \.self) { recipe in
+                    NavigationLink(destination: DetailView(recipe: recipe)) {
+                        Text(recipe.title!)
+
+                    }
+                }
+            }
+            .searchable(text: $searchText, prompt: "Look for something")
+            .onChange(of: searchText, perform: { newValue in
+                recipes.nsPredicate = newValue.isEmpty ? nil : NSPredicate(format: "title BEGINSWITH %@", newValue)
+            })
+            .navigationTitle("Search here!")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                        Button {
+                            dismiss()
+                        } label: {
+                            Label("Back", systemImage: "xmark")
+                        }
+                    }
+                }
+        }
     }
 }
 
