32e71ea6e7f38fdec0979f0b5466ee2825b6a671
Timea Bartha <timea.bartha.93@gmail.com>
2023-08-31 16:20:34 +0200

    Solved debug message when Closing app on a physical device
    The error was [13433:1227989] [Snapshotting] Snapshotting a view (0x102818800, UIKeyboardImpl) that is not in a visible window requires afterScreenUpdates:YES.
    Wrapped the preview ContentView() in a ZStack

--- Flavorly/Views/ContentView.swift
+++ Flavorly/Views/ContentView.swift
@@ -91,6 +91,8 @@ extension Color {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ZStack {
+            ContentView()
+        }
     }
 }
