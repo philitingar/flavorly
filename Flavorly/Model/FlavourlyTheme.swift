e09af0126777c341d3d16766d649ecab5a58b2c4
Timea Bartha <timea.bartha.93@gmail.com>
2024-06-29 12:08:24 +0200

    Added functions to have the current theme persist.
    the problem I was having was the the theme did not persist after the user closed the app.

--- Flavorly/Model/FlavourlyTheme.swift
+++ Flavorly/Model/FlavourlyTheme.swift
@@ -8,7 +8,7 @@
 import Foundation
 import SwiftUI
 
-struct Theme {
+struct Theme: Equatable {
     let backgroundColor: Color
     let textColor: Color
     let titleColor: Color
@@ -24,5 +24,42 @@ let lightOriginal = Theme(backgroundColor: .white, textColor: .black, titleColor
 let darkOriginal = Theme(backgroundColor: .black, textColor: .white, titleColor: FlavourlyColor.blue.light, searchIconColor: FlavourlyColor.blue.light, addIconColor: FlavourlyColor.green.light, deleteIconColor:FlavourlyColor.red.light, listBackgroundColor: FlavourlyColor.blue.light.opacity(0.3), secondaryColor: .secondary, sectionBackgroundColor: .gray.opacity(0.7), preferredColorScheme: .dark)
 
 class ThemeManager: ObservableObject {
-    @Published var currentTheme: Theme = darkOriginal
+    @Published var currentTheme: Theme = darkOriginal {
+        didSet {
+            saveTheme()
+        }
+    }
+    
+    init() {
+        loadTheme()
+    }
+    
+    private func saveTheme() {
+        let themeName: String
+        switch currentTheme {
+        case lightOriginal:
+            themeName = "lightOriginal"
+        case darkOriginal:
+            themeName = "darkOriginal"
+        default:
+            themeName = "lightOriginal" // Default or additional themes can be handled here
+        }
+        
+        UserDefaults.standard.set(themeName, forKey: "selectedTheme")
+    }
+    
+    private func loadTheme() {
+        if let savedTheme = UserDefaults.standard.string(forKey: "selectedTheme") {
+            switch savedTheme {
+            case "lightOriginal":
+                currentTheme = lightOriginal
+            case "darkOriginal":
+                currentTheme = darkOriginal
+            default:
+                currentTheme = lightOriginal // Default or additional themes can be handled here
+            }
+        } else {
+            currentTheme = lightOriginal // Default theme if nothing is saved
+        }
+    }
 }
