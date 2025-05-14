1c2edb75854535de164684d63d8976cfdc68d952
Timea B <timea.bartha.93@gmail.com>
2025-05-14 18:55:36 +0200

    ui change

--- Flavorly/Views/Search/RecipeListView.swift
+++ Flavorly/Views/Search/RecipeListView.swift
@@ -18,44 +18,68 @@ struct RecipeListView: View {
     
     var body: some View {
         NavigationView {
-            List {
-                ForEach(recipes, id: \.self) { recipe in
-                    NavigationLink(destination: DetailView(recipe: recipe))
-                    {
-                        Text(recipe.title!)
+            VStack(alignment: .center) {
+                Text("Here are the recipes matching your selected tags:")
+                    .font(.headline)
+                    .padding(2)
+                List {
+                    ForEach(recipes, id: \.self) { recipe in
+                        NavigationLink(destination: DetailView(recipe: recipe))
+                        {
+                            Text(recipe.title!)
+                        }
                     }
-                }.listRowBackground(Color.backgroundBlue.opacity(0.4))
-            }.onAppear {
-                let fetchRequest: NSFetchRequest<Recipe> = Recipe.fetchRequest()
-                var tagPredicates = [NSPredicate]()
-                tags.forEach { tag in
-                    let tagPredicate = NSPredicate(format: "ANY recipeToTag.id == %@", tag.id! as NSUUID)
-                    tagPredicates.append(tagPredicate)
+                    .listRowSeparatorTint(Color.backgroundBlue)
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 2, trailing: 0))
+                    .padding(4)
                 }
-                let compoundPredicate = NSCompoundPredicate(type: .and, subpredicates: tagPredicates)
-                fetchRequest.predicate = compoundPredicate
-                do {
-                    recipes = try moc.fetch(fetchRequest)
-                } catch {
-                    print("Error fetching recipes: \(error.localizedDescription)")
-                }
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button {
-                        dismiss()
-                    } label: {
-                        Image(systemName: "arrowshape.turn.up.backward.circle.fill")
-                            .foregroundStyle(Color.backgroundRed)
-                            .font(.title3)
+                .padding(.horizontal)
+                .scrollContentBackground(.hidden)
+                .listStyle(PlainListStyle())
+                .onAppear {
+                    let fetchRequest: NSFetchRequest<Recipe> = Recipe.fetchRequest()
+                    var tagPredicates = [NSPredicate]()
+                    tags.forEach { tag in
+                        let tagPredicate = NSPredicate(format: "ANY recipeToTag.id == %@", tag.id! as NSUUID)
+                        tagPredicates.append(tagPredicate)
+                    }
+                    let compoundPredicate = NSCompoundPredicate(type: .and, subpredicates: tagPredicates)
+                    fetchRequest.predicate = compoundPredicate
+                    do {
+                        recipes = try moc.fetch(fetchRequest)
+                    } catch {
+                        print("Error fetching recipes: \(error.localizedDescription)")
                     }
                 }
-            }
-            .overlay(Group {
-                if recipes.isEmpty {
-                    Text("no.recipe.to.tag.message").padding(5)
+                .overlay(Group {
+                    if recipes.isEmpty {
+                        if #available(iOS 17.0, *) {
+                            if #available(iOS 18.0, *) {
+                                ContentUnavailableView {
+                                    Label {
+                                        Text("no.recipe.to.tag.message")
+                                            .font(.headline)
+                                    } icon: {
+                                        Image(systemName: "book.pages")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 30, height: 30)
+                                    }
+                                    
+                                } description: {
+                                    Text("")
+                                }
+                                .symbolEffect(.bounce, options: .repeating)
+                                
+                            } else {
+                                Text("no.recipe.to.tag.message").padding(5)
+                            }
+                        }
+                    }
                 }
-            })
+                )
+            }
+            .padding(2)
         }
     }
 }
