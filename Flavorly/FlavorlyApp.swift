66cd426fd2b9528594c5d284d655c9ac87c2759f
Timea B <timea.bartha.93@gmail.com>
2025-08-12 17:33:16 +0200

    what's new added

--- Flavorly/FlavorlyApp.swift
+++ Flavorly/FlavorlyApp.swift
@@ -6,16 +6,17 @@
 //
 
 import SwiftUI
-
+private let lastVersionKey = "lastVersionLaunched"
 @main
 struct FlavorlyApp: App {
     @StateObject private var dataController = DataController()
     @StateObject var themeManager = ThemeManager()
     @State private var isContentLoaded = false
+    @State private var showWhatsNew = false
+    
     var body: some Scene {
         WindowGroup {
             ZStack {
-                
                 TabBarView()
                     .environment(\.managedObjectContext, dataController.container.viewContext)
                     .accentColor(themeManager.currentTheme.accentColor)
@@ -28,7 +29,26 @@ struct FlavorlyApp: App {
                     }
                 }
             }
+            .sheet(isPresented: $showWhatsNew) {
+                WhatsNewView()
+                    .presentationDetents([.medium])
+                    .presentationDragIndicator(.visible)
+            }
+            .onAppear {
+                checkVersion()
+            }
             .preferredColorScheme(themeManager.currentTheme.preferredColorScheme)
         }
     }
+    private func checkVersion() {
+            let currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? ""
+            let lastVersion = UserDefaults.standard.string(forKey: lastVersionKey) ?? ""
+            
+            if currentVersion != lastVersion {
+                // New version detected — show popup
+                showWhatsNew = true
+                // Save the current version
+                UserDefaults.standard.setValue(currentVersion, forKey: lastVersionKey)
+            }
+        }
 }
